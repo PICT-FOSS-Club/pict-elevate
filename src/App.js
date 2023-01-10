8c590d78634df1e960c828f7f6a64383a7e70083
D-WhiteDevil29 <99096080+D-WhiteDevil29@users.noreply.github.com>
2023-01-10 16:22:10 +0530

    Added paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './Pages/Home/Home';
-import { BrowserRouter, Routes, Route,  } from "react-router-dom";
+import { HashRouter, Routes, Route,  } from "react-router-dom";
 import Basketball from './Pages/Basketball/Basketball';
 import Volleyball from './Pages/Volleyball/Volleyball';
 import Team from './Pages/Team/Team';
@@ -9,14 +9,14 @@ import Team from './Pages/Team/Team';
 function App() {
   return (
     <>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="Team" element={<Team/>} />
           <Route path="Basketball" element={<Basketball />} />
           <Route path="Volleyball" element={<Volleyball />} />
       </Routes>
-    </BrowserRouter> 
+    </HashRouter> 
 
     </>
   );
